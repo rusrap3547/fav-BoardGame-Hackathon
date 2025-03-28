2d709b4a100d197d46dd0659c4601513b0e96d43
rusrap3547 <t3sserak@proton.me>
2025-03-28 13:33:19 -0400

    fixed comments

--- gamePieces.js
+++ gamePieces.js
@@ -2,83 +2,83 @@ const Marshal = {
   name: "Marshal",
   rank: "10",
   moves: 1,
-  //image:"./images/Marshal.png"
+  image:"./images/Marshal.png",
   quantity: 1,
 };
 const General = {
   name: "General",
   rank: "9",
   moves: 1,
-  //image:"./images/General.png"
+  image:"./images/General.png",
   quantity: 1,
 };
 const Colonel = {
   name: "Colonel",
   rank: "8",
   moves: 1,
-  //image:"./images/Colonel.png"
+  image:"./images/Colonel.png",
   quantity: 2,
 };
 const Major = {
   name: "Major",
   rank: "7",
   moves: 1,
-  //image:"./images/Major.png"
+  image:"./images/Major.png",
   quantity: 3,
 };
 const Captain = {
   name: "Captain",
   rank: "6",
   moves: 1,
-  //image:"./images/Captain.png"
+  image:"./images/Captain.png",
   quantity: 4,
 };
 const Lieutenant = {
   name: "Lieutenant",
   rank: "5",
   moves: 1,
-  //image:"./images/Lieutenant.png"
+  image:"./images/Lieutenant.png",
   quantity: 4,
 };
 const Sergeant = {
   name: "Sergeant",
   rank: "4",
   moves: 1,
-  //image:"./images/Sergeant.png"
+  image:"./images/Sergeant.png",
   quantity: 4,
 };
 const Miner = {
   name: "Miner",
   rank: "3",
   moves: 1,
-  //image:"./images/Miner.png"
+  image:"./images/Miner.png",
   quantity: 5,
 };
 const Scout = {
   name: "Scout",
   rank: "2",
   moves: "3",
-  //image:"./images/Scout.png"
+  image:"./images/Scout.png",
   quantity: 8,
 };
 const Spy = {
   name: "Spy",
   rank: "1",
   moves: 1,
-  //image:"./images/Spy.png"
+  image:"./images/Spy.png",
   quantity: 1,
 };
 const Bomb = {
   name: "Bomb",
   rank: "B",
   moves: 0,
-  //image:"./images/Bomb.png"
+  image:"./images/Bomb.png",
   quantity: 6,
 };
 const Flag = {
   name: "Flag",
   rank: "F",
   moves: 0,
-  //image:"./images/Flag.png"
+  image:"./images/Flag.png",
   quantity: 1,
 };
