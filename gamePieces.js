bc5f3350e6d62cbf610ca44bf68312a579488612
Shav101 <shavkat192710@gmail.com>
2025-03-31 22:21:39 -0400

    Changed index, added landing page, added css for landing page.

---
+++ gamePieces.js
@@ -0,0 +1,84 @@
+const Marshal = {
+  name: "Marshal",
+  rank: "10",
+  moves: 1,
+  //image:"./images/Marshal.png"
+  quantity: 1,
+};
+const General = {
+  name: "General",
+  rank: "9",
+  moves: 1,
+  //image:"./images/General.png"
+  quantity: 1,
+};
+const Colonel = {
+  name: "Colonel",
+  rank: "8",
+  moves: 1,
+  //image:"./images/Colonel.png"
+  quantity: 2,
+};
+const Major = {
+  name: "Major",
+  rank: "7",
+  moves: 1,
+  //image:"./images/Major.png"
+  quantity: 3,
+};
+const Captain = {
+  name: "Captain",
+  rank: "6",
+  moves: 1,
+  //image:"./images/Captain.png"
+  quantity: 4,
+};
+const Lieutenant = {
+  name: "Lieutenant",
+  rank: "5",
+  moves: 1,
+  //image:"./images/Lieutenant.png"
+  quantity: 4,
+};
+const Sergeant = {
+  name: "Sergeant",
+  rank: "4",
+  moves: 1,
+  //image:"./images/Sergeant.png"
+  quantity: 4,
+};
+const Miner = {
+  name: "Miner",
+  rank: "3",
+  moves: 1,
+  //image:"./images/Miner.png"
+  quantity: 5,
+};
+const Scout = {
+  name: "Scout",
+  rank: "2",
+  moves: "3",
+  //image:"./images/Scout.png"
+  quantity: 8,
+};
+const Spy = {
+  name: "Spy",
+  rank: "1",
+  moves: 1,
+  //image:"./images/Spy.png"
+  quantity: 1,
+};
+const Bomb = {
+  name: "Bomb",
+  rank: "B",
+  moves: 0,
+  //image:"./images/Bomb.png"
+  quantity: 6,
+};
+const Flag = {
+  name: "Flag",
+  rank: "F",
+  moves: 0,
+  //image:"./images/Flag.png"
+  quantity: 1,
+};
