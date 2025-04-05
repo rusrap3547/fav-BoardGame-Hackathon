4ef3d2d3cfb393a757b6b77915b81aa1d456e006
rusrap3547 <t3sserak@proton.me>
2025-04-05 16:48:10 -0400

    Added combatBoard and started movement

--- gamePieces.js
+++ gamePieces.js
@@ -1,85 +1,96 @@
-
 const Marshal = {
   name: "Marshal",
   rank: "10",
   moves: 1,
-  //image:"./images/Marshal.png"
+  playerImage: "./images/Stratego_B10.svg.png",
+  enemyImage: "./images/Stratego_R10.svg.png",
   quantity: 1,
 };
 const General = {
   name: "General",
   rank: "9",
   moves: 1,
-  //image:"./images/General.png"
+  playerImage: "./images/Stratego_B9.svg.png",
+  enemyImage: "./images/Stratego_R9.svg.png",
   quantity: 1,
 };
 const Colonel = {
   name: "Colonel",
   rank: "8",
   moves: 1,
-  //image:"./images/Colonel.png"
+  playerImage: "./images/Stratego_B8.svg.png",
+  enemyImage: "./images/Stratego_R8.svg.png",
   quantity: 2,
 };
 const Major = {
   name: "Major",
   rank: "7",
   moves: 1,
-  //image:"./images/Major.png"
+  playerImage: "./images/Stratego_B7.svg.png",
+  enemyImage: "./images/Stratego_R7.svg.png",
   quantity: 3,
 };
 const Captain = {
   name: "Captain",
   rank: "6",
   moves: 1,
-  //image:"./images/Captain.png"
+  playerImage: "./images/Stratego_B6.svg.png",
+  enemyImage: "./images/Stratego_R6.svg.png",
   quantity: 4,
 };
 const Lieutenant = {
   name: "Lieutenant",
   rank: "5",
   moves: 1,
-  //image:"./images/Lieutenant.png"
+  playerImage: "./images/Stratego_B5.svg.png",
+  enemyImage: "./images/Stratego_R5.svg.png",
   quantity: 4,
 };
 const Sergeant = {
   name: "Sergeant",
   rank: "4",
   moves: 1,
-  //image:"./images/Sergeant.png"
+  playerImage: "./images/Stratego_B4.svg.png",
+  enemyImage: "./images/Stratego_R4.svg.png",
   quantity: 4,
 };
 const Miner = {
   name: "Miner",
   rank: "3",
   moves: 1,
-  //image:"./images/Miner.png"
+  playerImage: "./images/Stratego_B3.svg.png",
+  enemyImage: "./images/Stratego_R3.svg.png",
   quantity: 5,
 };
 const Scout = {
   name: "Scout",
   rank: "2",
   moves: "3",
-  //image:"./images/Scout.png"
+  playerImage: "./images/Stratego_B2.svg.png",
+  enemyImage: "./images/Stratego_R2.svg.png",
   quantity: 8,
 };
 const Spy = {
   name: "Spy",
   rank: "1",
   moves: 1,
-  //image:"./images/Spy.png"
+  playerImage: "./images/Stratego_B1.svg.png",
+  enemyImage: "./images/Stratego_R1.svg.png",
   quantity: 1,
 };
 const Bomb = {
   name: "Bomb",
   rank: "B",
   moves: 0,
-  //image:"./images/Bomb.png"
+  playerImage: "./images/Stratego_BB.svg.png",
+  enemyImage: "./images/Stratego_RB.svg.png",
   quantity: 6,
 };
 const Flag = {
   name: "Flag",
   rank: "F",
   moves: 0,
-  //image:"./images/Flag.png"
+  playerImage: "./images/Stratego_BF.svg.png",
+  enemyImage: "./images/Stratego_RF.svg.png",
   quantity: 1,
 };
